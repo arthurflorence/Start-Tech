8587c17ca219b4e443d10abf211e6bf204e3130d
arthurflorence <arthur.florence2001@gmail.com>
2024-01-26 15:24:26 -0300

    exercico 4

---
+++ Sprint 6/Entregas/listagemCliente.java
@@ -0,0 +1,13 @@
+public class listagemCliente {
+    public static void main(String[] args) {
+
+        int anoFulano = 1980;
+        int anoSicrano = 2000;
+
+        System.out.println(">>> Listagem de Cliente" + "\n");
+        System.out.println("Nome: Fulano " + "\n" +
+                "Ano de Nascimento: " + anoFulano + "\n");
+        System.out.println("Nome: Sicrano " + "\n" +
+                "Ano de Nascimento: " + anoSicrano);
+    }
+}
