64635c7ca633695bdc5963b4251a0c8997eaf6c9
arthurflorence <arthur.florence2001@gmail.com>
2024-01-26 15:31:35 -0300

    exercico 5

---
+++ Sprint 6/Entregas/listagemProduto.java
@@ -0,0 +1,11 @@
+public class listagemProduto {
+    public static void main(String[] args) {
+
+        double valorLivro = 49.95;
+        double valorCelular = 1470.76;
+
+        System.out.println(">>> Listagem de Produtos" + "\n");
+        System.out.println("Nome: Livro " + "\n" +
+                "Preço: R$ " + valorLivro + "\n");
+        System.out.println("Nome: Celular " + "\n" +
+                "Preço: R$ " + valorCelular);
